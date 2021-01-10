31a1cb7bbe3d36368c63142d8bb7fbf8e09508d2
radus <radumstoica@gmail.com>
2021-01-10 14:42:28 +0200

    parity problem solved

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,10 +1,21 @@
 from test_framework import generic_test
 
+def simple_parity(x: int) -> int:
+    result = 0
+    while x:
+        result ^= x & 1 
+        x >>= 1
+    return result
 
+cache = [simple_parity(i) for i in range(65536)]
+    
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
-
+    result = 0
+    while x:
+        last_bits = x & 0xFFFF
+        result ^= cache[last_bits]
+        x >>=  16
+    return result
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
